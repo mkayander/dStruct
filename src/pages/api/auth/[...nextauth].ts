9fe988449f5b4dab090c482ee20d7b79bfd9a414
mkayander <maxim.kayander1@gmail.com>
2022-08-25 22:53:10 +0200

    feat: Added Google, Email, and a test Credentials provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
-// import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from 'next-auth/providers/credentials';
+import EmailProvider from 'next-auth/providers/email';
+import GoogleProvider from 'next-auth/providers/google';
 // import FacebookProvider from "next-auth/providers/facebook"
 import GithubProvider from 'next-auth/providers/github';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
@@ -15,13 +17,38 @@ export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     // https://next-auth.js.org/configuration/providers/oauth
     providers: [
-        /* EmailProvider({
-             server: process.env.EMAIL_SERVER,
-             from: process.env.EMAIL_FROM,
-           }),
-        // Temporarily removing the Apple provider from the demo site as the
-        // callback URL for it needs updating due to Vercel changing domains
+        CredentialsProvider({
+            // The name to display on the sign-in form (e.g. "Sign in with...")
+            name: 'Credentials',
+            // The credentials are used to generate a suitable form on the sign-in page.
+            // You can specify whatever fields you are expecting to be submitted.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+                username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+                password: { label: 'Password', type: 'password' },
+            },
+            async authorize(credentials, req) {
+                console.log(credentials, req);
+                // Add logic here to look up the user from the credentials supplied
+                const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' };
 
+                if (user) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return user;
+                } else {
+                    // If you return null then an error will be displayed advising the user to check their details.
+                    return null;
+
+                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                }
+            },
+        }),
+        EmailProvider({
+            server: process.env.EMAIL_SERVER,
+            from: process.env.EMAIL_FROM,
+        }),
+        /*
         Providers.Apple({
           clientId: process.env.APPLE_ID,
           clientSecret: {
@@ -40,10 +67,10 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GITHUB_ID!,
             clientSecret: process.env.GITHUB_SECRET!,
         }),
-        // GoogleProvider({
-        //     clientId: process.env.GOOGLE_ID,
-        //     clientSecret: process.env.GOOGLE_SECRET,
-        // }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID!,
+            clientSecret: process.env.GOOGLE_SECRET!,
+        }),
         // TwitterProvider({
         //     clientId: process.env.TWITTER_ID,
         //     clientSecret: process.env.TWITTER_SECRET,
