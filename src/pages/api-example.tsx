cd78ced462bc9728043778ba3750f58fd3d2a8ec
mkayander <maxim.kayander1@gmail.com>
2022-09-04 23:37:08 +0200

    feat: Added Main layout usage

--- src/pages/api-example.tsx
+++ src/pages/api-example.tsx
@@ -1,37 +1,43 @@
+import { NextPage } from 'next';
 import { Box, CircularProgress, Container, Typography } from '@mui/material';
 import { trpc } from '@src/utils';
+import { MainLayout } from '@src/layouts';
 
-export default function ApiExamplePage() {
+const ApiExamplePage: NextPage = () => {
     const avatar = trpc.useQuery(['avatar']);
 
     return (
-        <Container>
-            <Box
-                sx={{
-                    my: 4,
-                }}
-            >
-                <Typography variant="h2">API Example</Typography>
-                <p>The examples below show responses from the example API endpoints.</p>
-                <p>
-                    <em>You must be signed in to see responses.</em>
-                </p>
-                <h2>Session</h2>
-                <p>/api/examples/session</p>
-                <iframe src="/api/examples/session" />
-                <h2>JSON Web Token</h2>
-                <p>/api/examples/jwt</p>
-                <iframe src="/api/examples/jwt" />
+        <MainLayout>
+            <Container>
+                <Box
+                    sx={{
+                        my: 4,
+                    }}
+                >
+                    <Typography variant="h2">API Example</Typography>
+                    <p>The examples below show responses from the example API endpoints.</p>
+                    <p>
+                        <em>You must be signed in to see responses.</em>
+                    </p>
+                    <h2>Session</h2>
+                    <p>/api/examples/session</p>
+                    <iframe src="/api/examples/session" />
+                    <h2>JSON Web Token</h2>
+                    <p>/api/examples/jwt</p>
+                    <iframe src="/api/examples/jwt" />
 
-                <h2>Hello World</h2>
-                <p>/api/examples/hello</p>
-                <iframe src="/api/examples/hello" />
+                    <h2>Hello World</h2>
+                    <p>/api/examples/hello</p>
+                    <iframe src="/api/examples/hello" />
 
-                <h2>Protected Avatar</h2>
-                <p>TRPC</p>
-                {avatar.isError && JSON.stringify(avatar.error, null, 2)}
-                {!avatar.isLoading ? avatar.data : <CircularProgress variant="indeterminate" />}
-            </Box>
-        </Container>
+                    <h2>Protected Avatar</h2>
+                    <p>TRPC</p>
+                    {avatar.isError && JSON.stringify(avatar.error, null, 2)}
+                    {!avatar.isLoading ? avatar.data : <CircularProgress variant="indeterminate" />}
+                </Box>
+            </Container>
+        </MainLayout>
     );
-}
+};
+
+export default ApiExamplePage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import React, { ChangeEventHandler, useState } from 'react';
 import type { NextPage } from 'next';
 import { trpc } from '@src/utils/trpc';
-import { ProblemCard, SessionWidget } from '@src/components';
 import { Box, Button, CircularProgress, Container, Link, TextField, Typography } from '@mui/material';
 import { useGetUserProfileLazyQuery } from '@src/graphql/generated';
+import { ProblemCard, SessionWidget } from '@src/components';
+import { MainLayout } from '@src/layouts';
 
 const Home: NextPage = () => {
     const { data: problemsList, refetch } = trpc.useQuery(['problem.all']);
@@ -20,49 +21,56 @@ const Home: NextPage = () => {
     };
 
     return (
-        <Container>
-            <Box
-                sx={{
-                    my: 4,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}
-            >
-                <Typography variant="h4" component="h1" gutterBottom>
-                    MUI v5 + Next.js with TypeScript example
-                </Typography>
-                <Link href="/" color="secondary">
-                    Go to the about page
-                </Link>
-                <Link href="/api-example">Go to the api test page</Link>
-                {problemsList?.map((item) => (
-                    <ProblemCard key={item.id} data={item} />
-                ))}
-                <Button type="button" variant="contained" onClick={() => refetch()}>
-                    Refetch
-                </Button>
-                <br />
+        <MainLayout>
+            <Container>
+                <Box
+                    sx={{
+                        my: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography variant="h4" component="h1" gutterBottom>
+                        MUI v5 + Next.js with TypeScript example
+                    </Typography>
+                    <Link href="/" color="secondary">
+                        Go to the about page
+                    </Link>
+                    <Link href="/api-example">Go to the api test page</Link>
+                    {problemsList?.map((item) => (
+                        <ProblemCard key={item.id} data={item} />
+                    ))}
+                    <Button type="button" variant="contained" onClick={() => refetch()}>
+                        Refetch
+                    </Button>
+                    <br />
 
-                <SessionWidget />
+                    <SessionWidget />
 
-                {/*<ProTip />*/}
-                {/*<Copyright />*/}
+                    {/*<ProTip />*/}
+                    {/*<Copyright />*/}
 
-                <br />
+                    <br />
 
-                <TextField variant="outlined" label="Username" onChange={handleUsernameChange} error={Boolean(error)} />
-                <Button onClick={handleLaunchQuery}>Launch query</Button>
-                {loading ? (
-                    <CircularProgress />
-                ) : (
-                    <div>
-                        <pre>{JSON.stringify(error || userProfile, null, 4)}</pre>
-                    </div>
-                )}
-            </Box>
-        </Container>
+                    <TextField
+                        variant="outlined"
+                        label="Username"
+                        onChange={handleUsernameChange}
+                        error={Boolean(error)}
+                    />
+                    <Button onClick={handleLaunchQuery}>Launch query</Button>
+                    {loading ? (
+                        <CircularProgress />
+                    ) : (
+                        <div>
+                            <pre>{JSON.stringify(error || userProfile, null, 4)}</pre>
+                        </div>
+                    )}
+                </Box>
+            </Container>
+        </MainLayout>
     );
 };
 
