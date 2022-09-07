779075aed18097c5f511179caf4b229eba0df924
mkayander <maxim.kayander1@gmail.com>
2022-09-07 23:10:28 +0200

    feat: Provide bucket base url to public runtime config.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    publicRuntimeConfig: {
+        BUCKET_BASE_URL: process.env.BUCKET_BASE_URL,
+    },
+    images: {
+        domains: [new URL(process.env.BUCKET_BASE_URL).hostname],
+    },
 };
 
 module.exports = nextConfig;
