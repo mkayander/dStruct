f8cf177bf640419c4bc549c03d1b1fd59babafd0
mkayander <maxim.kayander1@gmail.com>
2022-09-12 00:40:57 +0200

    feat: Added Formik usage on dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,15 +1,16 @@
-import { Box, Button, CircularProgress, Container, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
 import { trpc } from '@src/utils';
 import { MainLayout } from '@src/layouts';
 import { useSession } from 'next-auth/react';
-import { useState } from 'react';
+import { Box, Button, CircularProgress, Container, Typography } from '@mui/material';
 import { useGetUserProfileLazyQuery } from '@src/graphql/generated';
+import { Formik, Form, Field } from 'formik';
+import { Autocomplete, TextField, Select, Switch, ToggleButtonGroup } from 'formik-mui';
 
 export default function DashboardPage() {
     const session = useSession();
-    const [userNameInput, setUserNameInput] = useState<string>();
 
-    const [getUserProfile, { loading, error }] = useGetUserProfileLazyQuery();
+    const [getUserProfile, { loading: gqlLoading, error }] = useGetUserProfileLazyQuery();
 
     const userId = session.data?.user.id;
 
@@ -22,26 +23,18 @@ export default function DashboardPage() {
     });
 
     const linkUser = trpc.useMutation(['leetcode.linkUser']);
+    const unlinkUser = trpc.useMutation(['leetcode.unlinkUser']);
+
+    const loading = gqlLoading || linkUser.isLoading || unlinkUser.isLoading;
+
+    const handleLinkedUserReset = async () => {
+        await unlinkUser.mutate();
+        await refetch();
+    };
 
     if (!session.data) {
         return <CircularProgress />;
     }
-
-    const handleSubmit = async () => {
-        if (!userNameInput) return;
-
-        const { data } = await getUserProfile({ variables: { username: userNameInput } });
-        if (!data?.matchedUser) return;
-
-        const { userAvatar } = data.matchedUser.profile;
-
-        await linkUser.mutate({
-            username: userNameInput,
-            userAvatar: userAvatar || 'none',
-        });
-
-        await refetch();
-    };
 
     return (
         <MainLayout isLoading={isLoading}>
@@ -57,32 +50,71 @@ export default function DashboardPage() {
                         <Box>
                             <Typography>Your LeetCode account name:</Typography>
                             <Typography>{user.leetCodeUsername}</Typography>
+                            <Button
+                                disabled={unlinkUser.isLoading}
+                                color="error"
+                                variant="outlined"
+                                onClick={handleLinkedUserReset}
+                            >
+                                Reset
+                            </Button>
                         </Box>
                     ) : (
-                        <Box
-                            sx={{
-                                '& > *': {
-                                    marginTop: '12px',
-                                },
+                        <Formik
+                            initialValues={{
+                                username: '',
+                            }}
+                            validate={async (values) => {
+                                const errors: { username?: string } = {};
+                                if (!values.username) {
+                                    errors.username = 'Required';
+                                } else {
+                                }
+                                return errors;
+                            }}
+                            onSubmit={async ({ username }, { setErrors }) => {
+                                const { data } = await getUserProfile({ variables: { username: username } });
+                                if (!data?.matchedUser) {
+                                    setErrors({ username: 'No user with given username found!' });
+                                    return;
+                                }
+
+                                const { userAvatar } = data.matchedUser.profile;
+
+                                await linkUser.mutate({
+                                    username,
+                                    userAvatar: userAvatar || 'none',
+                                });
+
+                                await refetch();
                             }}
                         >
-                            {/*<Typography>Please enter your LeetCode account name:</Typography>*/}
-                            <TextField
-                                label="Username"
-                                onChange={(event) => setUserNameInput(event.currentTarget.value)}
-                                error={Boolean(error)}
-                            />
-                            <Button
-                                type="button"
-                                variant="outlined"
-                                color="primary"
-                                onClick={handleSubmit}
-                                sx={{ display: 'block' }}
-                            >
-                                Submit!
-                            </Button>
-                            {loading && <CircularProgress variant="indeterminate" />}
-                        </Box>
+                            {({ submitForm, isSubmitting, touched, errors }) => (
+                                <Box>
+                                    <Typography>Please enter your LeetCode account name:</Typography>
+                                    <Field
+                                        component={TextField}
+                                        name="username"
+                                        type="text"
+                                        label="Username"
+                                        required
+                                        helperText="LeetCode Username"
+                                        // error={errors['username']}
+                                    />
+
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        disabled={isSubmitting}
+                                        onClick={submitForm}
+                                        sx={{ display: 'block' }}
+                                    >
+                                        Submit!
+                                    </Button>
+                                    {loading && <CircularProgress variant="indeterminate" />}
+                                </Box>
+                            )}
+                        </Formik>
                     )}
                 </Box>
             </Container>
