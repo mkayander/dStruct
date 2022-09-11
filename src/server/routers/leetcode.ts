b56e7518a6b4c534f55f7939e2fa5b2e549b4e64
mkayander <maxim.kayander1@gmail.com>
2022-09-12 00:40:20 +0200

    feat: Added unlink user operation

--- src/server/routers/leetcode.ts
+++ src/server/routers/leetcode.ts
@@ -1,23 +1,36 @@
 import { createRouter } from '../createRouter';
 import { z } from 'zod';
 
-export const leetcodeRouter = createRouter().mutation('linkUser', {
-    input: z.object({
-        username: z.string(),
-        userAvatar: z.string(),
-    }),
-    async resolve({ input, ctx }) {
-        const { username, userAvatar } = input;
-        return await ctx.prisma.user.update({
-            where: { id: ctx.session?.user.id },
-            data: {
-                leetCode: {
-                    create: {
-                        username,
-                        userAvatar,
+export const leetcodeRouter = createRouter()
+    .mutation('linkUser', {
+        input: z.object({
+            username: z.string(),
+            userAvatar: z.string(),
+        }),
+        async resolve({ input, ctx }) {
+            const { username, userAvatar } = input;
+            return await ctx.prisma.user.update({
+                where: { id: ctx.session?.user.id },
+                data: {
+                    leetCode: {
+                        create: {
+                            username,
+                            userAvatar,
+                        },
                     },
                 },
-            },
-        });
-    },
-});
+            });
+        },
+    })
+    .mutation('unlinkUser', {
+        async resolve({ ctx }) {
+            return await ctx.prisma.user.update({
+                where: { id: ctx.session?.user.id },
+                data: {
+                    leetCode: {
+                        delete: true,
+                    },
+                },
+            });
+        },
+    });
