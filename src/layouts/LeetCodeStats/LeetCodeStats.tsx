27f6de0f181871480044fb04e65282b56ccbb87e
mkayander <maxim.kayander1@gmail.com>
2022-09-20 22:17:23 +0200

    feat: Added section components

---
+++ src/layouts/LeetCodeStats/LeetCodeStats.tsx
@@ -0,0 +1,11 @@
+import { DataSection } from '@src/layouts';
+import { Code } from '@mui/icons-material';
+import React from 'react';
+
+export const LeetCodeStats = () => {
+    return (
+        <DataSection title={'LeetCode Stats'} Icon={Code}>
+            <div></div>
+        </DataSection>
+    );
+};
