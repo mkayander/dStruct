05d2ad6d9da345e5930c858fc53a7c64894ef9ee
mkayander <maxim.kayander1@gmail.com>
2022-10-02 21:33:31 +0200

    feat: Added Rating Buttons component

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,4 @@ export { DataSection } from './DataSection/DataSection';
 export { TopicTag } from './TopicTag/TopicTag';
 export { QuestionSummary } from './QuestionSummary/QuestionSummary';
 export { CircularPercentage } from './CircularPercentage/CircularPercentage';
+export { RatingButtons } from './RatingButtons/RatingButtons';
