6588db4b0931643c09d2c696e39ba842eaacf141
mkayander <maxim.kayander1@gmail.com>
2022-10-05 23:14:12 +0200

    fix: Fixed Type Error when building the project

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,15 +1,15 @@
-import NextAuth from 'next-auth';
-import { User as PrismaUser } from '.prisma/client';
+import NextAuth, { DefaultSession } from 'next-auth';
+import { User as PrismaUser } from '@prisma/client';
 
 declare module 'next-auth' {
+    // export interface User extends PrismaUser {}
+
     /**
      * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
      */
     interface Session {
-        user: User;
+        user: PrismaUser & DefaultSession['user'];
     }
 
     type UserProps = PrismaUser;
-
-    export interface User extends PrismaUser {}
 }
