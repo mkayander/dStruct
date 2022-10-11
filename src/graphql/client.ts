d480732d20c2acba4eff539e2b7863cd0a524240
mkayander <maxim.kayander1@gmail.com>
2022-10-11 23:39:41 +0200

    feat: Added GraphQL scalars and session cookie parsing

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,6 +1,51 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { getCookie, setCookie } from 'cookies-next';
+
+const httpLink = createHttpLink({
+    uri: '/api/graphql',
+});
+
+const testCookie = '';
+
+setCookie('LEETCODE_SESSION', testCookie);
+
+const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = getCookie('LEETCODE_SESSION');
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            extToken: token,
+        },
+    };
+});
+
+const jsonParseRead = (field: string) => JSON.parse(field);
+
+const cache = new InMemoryCache({
+    typePolicies: {
+        QuestionNode: {
+            fields: {
+                stats: {
+                    read: jsonParseRead,
+                },
+                similarQuestions: {
+                    read: jsonParseRead,
+                },
+                envInfo: {
+                    read: jsonParseRead,
+                },
+                metaData: {
+                    read: jsonParseRead,
+                },
+            },
+        },
+    },
+});
 
 export const client = new ApolloClient({
-    uri: '/api/graphql',
-    cache: new InMemoryCache(),
+    link: authLink.concat(httpLink),
+    cache,
 });
