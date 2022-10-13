17f77df1df1958f956ba14c4b6cc12416c54c4a2
mkayander <maxim.kayander1@gmail.com>
2022-10-13 21:51:56 +0200

    feat: Moved to MySql database provider

--- prisma/migrations/20220716223440_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "ProblemList" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "solved" BOOLEAN DEFAULT false,
-
-    CONSTRAINT "ProblemList_pkey" PRIMARY KEY ("id")
-);
