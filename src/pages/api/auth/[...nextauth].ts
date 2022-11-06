cf8aed3341efa1d6e5892c15048c7e5bc9132493
mkayander <maxim.kayander1@gmail.com>
2022-11-06 21:31:06 +0100

    fix: Fixed user id type

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -31,7 +31,7 @@ export const authOptions: NextAuthOptions = {
             async authorize(credentials, req) {
                 console.log(credentials, req);
                 // Add logic here to look up the user from the credentials supplied
-                const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' };
+                const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' };
 
                 if (user) {
                     // Any object returned will be saved in `user` property of the JWT
