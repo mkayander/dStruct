b1cb9615812a94dc11601863b5f9eb1d98a91793
mkayander <maxim.kayander1@gmail.com>
2022-11-27 18:31:41 +0100

    feat: Added account page stub

---
+++ src/pages/account.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+const AccountPage: React.FC = () => {
+  return (
+    <Box>
+      <h1>Account page</h1>
+    </Box>
+  );
+};
+
+export default AccountPage;
