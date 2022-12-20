f14df8ecbbe2562e96bba96da0e83bdec3c2c3ad
mkayander <maxim.kayander1@gmail.com>
2022-12-20 21:45:21 +0100

    fix: Insert slash between S3 base url and bucket path

--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -1,4 +1,4 @@
 import { clientEnv as env } from '#/env/schema.mjs';
 
 export const getImageUrl = (relativePath: string) =>
-  `${env.NEXT_PUBLIC_BUCKET_BASE_URL}${relativePath}`;
+  `${env.NEXT_PUBLIC_BUCKET_BASE_URL}/${relativePath}`;
