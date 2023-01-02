7b281c1dbf2ce93e237ccae81b48749cc82c4e5d
mkayander <maxim.kayander1@gmail.com>
2023-01-02 22:24:07 +0100

    feat: Added debounce hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,28 @@
+import { useEffect, useMemo, useState } from 'react';
+
+type DebounceResult<T> = {
+  value: T;
+  isPending: boolean;
+};
+
+export const useDebounce = <T>(value: T, delay: number): DebounceResult<T> => {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+  const [isPending, setIsPending] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+    const timeoutId = setTimeout(() => {
+      setDebouncedValue(value);
+      setIsPending(false);
+    }, delay);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [value, delay]);
+
+  return useMemo(
+    () => ({ value: debouncedValue, isPending }),
+    [debouncedValue, isPending]
+  );
+};
