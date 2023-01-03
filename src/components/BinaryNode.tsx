4860c8184ba31552c01619ed7b668b6d60a22e61
mkayander <maxim.kayander1@gmail.com>
2023-01-03 21:55:36 +0100

    feat: Updated binary tree node component

--- src/components/BinaryNode.tsx
+++ src/components/BinaryNode.tsx
@@ -1,7 +1,17 @@
-import { Box, useTheme } from '@mui/material';
+import { Box, type SxProps, type Theme, useTheme } from '@mui/material';
 import React from 'react';
 
 import type { BinaryTreeNode } from '#/hooks/useBinaryTree';
+
+const nodeProps: SxProps<Theme> = {
+  display: 'flex',
+  justifyContent: 'center',
+  p: 1,
+  width: '42px',
+  height: '42px',
+};
+
+const GapElement = () => <Box sx={{ ...nodeProps, pointerEvents: 'none' }} />;
 
 type BinaryNodeProps = BinaryTreeNode;
 
@@ -9,8 +19,12 @@ export const BinaryNode: React.FC<BinaryNodeProps> = ({
   value,
   left,
   right,
+  meta,
 }: BinaryNodeProps) => {
   const theme = useTheme();
+
+  const gapsActive =
+    (meta.rootNode?.meta.maxDepth ?? 0) - meta.depth >= 1 || null;
 
   return (
     <Box
@@ -23,16 +37,17 @@ export const BinaryNode: React.FC<BinaryNodeProps> = ({
       }}
     >
       <Box
+        boxShadow={6}
         sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          p: 1,
-          width: '42px',
-          height: '42px',
+          ...nodeProps,
           borderRadius: '50%',
-          // background: theme.palette.action.hover,
           background: theme.palette.primary.main,
           border: `1px solid ${theme.palette.primary.light}`,
+          color: theme.palette.primary.contrastText,
+          transition: 'background .2s',
+          '&:hover': {
+            background: theme.palette.primary.light,
+          },
         }}
       >
         {value}
@@ -46,8 +61,8 @@ export const BinaryNode: React.FC<BinaryNodeProps> = ({
           gap: 4,
         }}
       >
-        {left && <BinaryNode {...left} />}
-        {right && <BinaryNode {...right} />}
+        {left ? <BinaryNode {...left} /> : gapsActive && <GapElement />}
+        {right ? <BinaryNode {...right} /> : gapsActive && <GapElement />}
       </Box>
     </Box>
   );
