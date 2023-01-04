d73cd9d46b2f4ac32b06d75c7769f7af75638080
mkayander <maxim.kayander1@gmail.com>
2023-01-04 15:20:18 +0100

    feat: Updated node styles. Use archer element.

--- src/components/BinaryNode.tsx
+++ src/components/BinaryNode.tsx
@@ -1,5 +1,7 @@
-import { Box, type SxProps, type Theme, useTheme } from '@mui/material';
+import { alpha, Box, type SxProps, type Theme, useTheme } from '@mui/material';
 import React from 'react';
+import { ArcherElement } from 'react-archer';
+import type { RelationType } from 'react-archer/lib/types';
 
 import type { BinaryTreeNode } from '#/hooks/useBinaryTree';
 
@@ -26,32 +28,79 @@ export const BinaryNode: React.FC<BinaryNodeProps> = ({
   const gapsActive =
     (meta.rootNode?.meta.maxDepth ?? 0) - meta.depth >= 1 || null;
 
+  const relations: RelationType[] = [];
+
+  if (left)
+    relations.push({
+      targetId: left.meta.id,
+      targetAnchor: 'middle',
+      sourceAnchor: 'middle',
+      style: {
+        endShape: {
+          circle: {
+            radius: 3,
+          },
+        },
+      },
+    });
+
+  if (right)
+    relations.push({
+      targetId: right.meta.id,
+      targetAnchor: 'middle',
+      sourceAnchor: 'middle',
+    });
+
   return (
     <Box
       sx={{
         display: 'flex',
+        position: 'relative',
+        zIndex: 1,
         flexFlow: 'column nowrap',
         alignItems: 'center',
         width: 'fit-content',
         gap: 2,
       }}
     >
+      <ArcherElement id={meta.id} relations={relations}>
+        <Box
+          sx={{
+            ...nodeProps,
+            borderRadius: '50%',
+            background: alpha(theme.palette.primary.main, 0.3),
+            border: `1px solid ${alpha(theme.palette.primary.light, 0.1)}`,
+            backdropFilter: 'blur(4px)',
+            boxShadow: `0px 0px 18px -2px ${alpha(
+              theme.palette.primary.dark,
+              0.5
+            )}`,
+            color: theme.palette.primary.contrastText,
+            transition: 'background .2s',
+
+            '&:hover': {
+              background: alpha(theme.palette.primary.light, 0.4),
+            },
+          }}
+        >
+          {value}
+        </Box>
+      </ArcherElement>
+
       <Box
-        boxShadow={6}
         sx={{
-          ...nodeProps,
+          position: 'absolute',
+          zIndex: -1,
+          top: '13px',
+          left: '50%',
+          transform: 'translate(-50%, 0)',
+          width: '18px',
+          height: '18px',
           borderRadius: '50%',
           background: theme.palette.primary.main,
-          border: `1px solid ${theme.palette.primary.light}`,
-          color: theme.palette.primary.contrastText,
-          transition: 'background .2s',
-          '&:hover': {
-            background: theme.palette.primary.light,
-          },
         }}
-      >
-        {value}
-      </Box>
+      />
+
       <Box
         sx={{
           display: 'flex',
