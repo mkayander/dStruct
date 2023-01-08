cdb4d63c23e3a83dd76763be079b7f89955896e2
mkayander <maxim.kayander1@gmail.com>
2023-01-08 20:55:02 +0100

    feat: Dispatch to store on tree build

--- src/hooks/useBinaryTree.ts
+++ src/hooks/useBinaryTree.ts
@@ -1,7 +1,9 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 
 import { isNumber } from '#/utils';
 import uuid4 from 'short-uuid';
+import { useDispatch } from 'react-redux';
+import { treeNodeSlice } from '#/store/reducers/treeNode';
 
 // [3,9,20,null,null,15,7]
 
@@ -30,54 +32,60 @@ export class BinaryTreeNode {
   }
 }
 
-const createChildNode = (value: number | null | undefined, meta: Partial<NodeMeta>) => {
-  if (!isNumber(value)) return null;
-
-
-  return new BinaryTreeNode(value, null, null, <NodeMeta>{ id: uuid4.generate(), ...meta });
-};
-
-const buildBinaryTree = (input?: BinaryTreeInput): BinaryTreeNode | null => {
-  if (!input || input.length === 0) return null;
-
-  const rootNum = input[0];
-  if (!isNumber(rootNum)) return null;
-  const root = new BinaryTreeNode(rootNum, null, null, {
-    id: uuid4.generate(),
-    depth: 0,
-    isRoot: true
-  });
-  const queue: BinaryTreeNode[] = [root];
-
-  let i = 1;
-
-  while (queue.length > 0 && i < input.length) {
-    const current = queue.shift();
-    if (!current) break;
-
-    const newDepth = current.meta.depth + 1;
-
-    const metaProps: Partial<NodeMeta> = {
-      depth: newDepth,
-      rootNode: root
-    };
-
-    root.meta.maxDepth = newDepth;
-
-    current.left = createChildNode(input[i], metaProps);
-    current.left && queue.push(current.left);
-    i++;
-
-    current.right = createChildNode(input[i], metaProps);
-    current.right && queue.push(current.right);
-    i++;
-  }
-
-  return root;
-};
-
 export const useBinaryTree = (
   input?: BinaryTreeInput
 ): BinaryTreeNode | null => {
-  return useMemo(() => buildBinaryTree(input), [input]);
+  const dispatch = useDispatch();
+
+  const createChildNode = useCallback((value: number | null | undefined, meta: Partial<NodeMeta>) => {
+    if (!isNumber(value)) return null;
+
+    const newNode = new BinaryTreeNode(value, null, null, <NodeMeta>{ id: uuid4.generate(), ...meta });
+
+    dispatch(treeNodeSlice.actions.add({
+      id: newNode.meta.id,
+      value: newNode.val
+    }));
+
+    return newNode;
+  }, [dispatch]);
+
+  return useMemo(() => {
+    if (!input || input.length === 0) return null;
+
+    const rootNum = input[0];
+    if (!isNumber(rootNum)) return null;
+    const root = new BinaryTreeNode(rootNum, null, null, {
+      id: uuid4.generate(),
+      depth: 0,
+      isRoot: true
+    });
+    const queue: BinaryTreeNode[] = [root];
+
+    let i = 1;
+
+    while (queue.length > 0 && i < input.length) {
+      const current = queue.shift();
+      if (!current) break;
+
+      const newDepth = current.meta.depth + 1;
+
+      const metaProps: Partial<NodeMeta> = {
+        depth: newDepth,
+        rootNode: root
+      };
+
+      root.meta.maxDepth = newDepth;
+
+      current.left = createChildNode(input[i], metaProps);
+      current.left && queue.push(current.left);
+      i++;
+
+      current.right = createChildNode(input[i], metaProps);
+      current.right && queue.push(current.right);
+      i++;
+    }
+
+    return root;
+  }, [createChildNode, input]);
 };
