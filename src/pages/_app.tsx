55b52a9dea64e8b3ea54fcb2ecc406e33441503a
mkayander <maxim.kayander1@gmail.com>
2023-01-08 20:52:19 +0100

    feat: Initialized Redux store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { SessionProvider } from 'next-auth/react';
 import Head from 'next/head';
 import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 import { type ReactElement, useMemo, useState } from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
 
 import { apolloClient } from '#/graphql/apolloClient';
 import { MainLayout as DefaultLayout } from '#/layouts/MainLayout';
@@ -13,14 +14,16 @@ import { themes } from '#/themes';
 import type { AppTypeWithLayout } from '#/types/page';
 import { trpc } from '#/utils';
 
+import { wrapper } from '#/store/makeStore';
 import '#/styles/globals.css';
 
 import 'overlayscrollbars/overlayscrollbars.css';
 
 const MyApp: AppTypeWithLayout<{ session: Session | null }> = ({
   Component,
-  pageProps: { session, ...pageProps },
+  ...restProps
 }) => {
+  const { store, props } = wrapper.useWrappedStore(restProps);
   // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
   const [darkMode, setDarkMode] = useState(true);
@@ -37,42 +40,44 @@ const MyApp: AppTypeWithLayout<{ session: Session | null }> = ({
     ));
 
   return (
-    <SessionProvider session={session}>
-      <ApolloProvider client={apolloClient}>
-        <ThemeProvider theme={theme}>
-          <Head>
-            <title>LeetPal</title>
-          </Head>
-          <CssBaseline />
-          <Box
-            sx={{
-              '.os-theme-dark.os-scrollbar > .os-scrollbar-track > .os-scrollbar-handle':
-                {
-                  background: theme.palette.action.hover,
-                  '&:hover': {
-                    backgroundColor: alpha(theme.palette.primary.light, 0.3),
+    <ReduxProvider store={store}>
+      <SessionProvider session={restProps.pageProps.session}>
+        <ApolloProvider client={apolloClient}>
+          <ThemeProvider theme={theme}>
+            <Head>
+              <title>LeetPal</title>
+            </Head>
+            <CssBaseline />
+            <Box
+              sx={{
+                '.os-theme-dark.os-scrollbar > .os-scrollbar-track > .os-scrollbar-handle':
+                  {
+                    background: theme.palette.action.hover,
+                    '&:hover': {
+                      backgroundColor: alpha(theme.palette.primary.light, 0.3),
+                    },
+                    '&:active': {
+                      backgroundColor: alpha(theme.palette.primary.dark, 0.6),
+                    },
                   },
-                  '&:active': {
-                    backgroundColor: alpha(theme.palette.primary.dark, 0.6),
-                  },
-                },
-            }}
-          >
-            <OverlayScrollbarsComponent
-              defer
-              style={{ height: '100vh' }}
-              options={{
-                scrollbars: {
-                  autoHide: 'scroll',
-                },
               }}
             >
-              {getLayout(<Component {...pageProps} />)}
-            </OverlayScrollbarsComponent>
-          </Box>
-        </ThemeProvider>
-      </ApolloProvider>
-    </SessionProvider>
+              <OverlayScrollbarsComponent
+                defer
+                style={{ height: '100vh' }}
+                options={{
+                  scrollbars: {
+                    autoHide: 'scroll',
+                  },
+                }}
+              >
+                {getLayout(<Component {...props.pageProps} />)}
+              </OverlayScrollbarsComponent>
+            </Box>
+          </ThemeProvider>
+        </ApolloProvider>
+      </SessionProvider>
+    </ReduxProvider>
   );
 };
 
