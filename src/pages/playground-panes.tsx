4612cad7857e6f750539e8bbe6e4429f7ebc3443
mkayander <maxim.kayander1@gmail.com>
2023-01-15 22:30:09 +0100

    feat: Use panel layout components

--- src/pages/playground-panes.tsx
+++ src/pages/playground-panes.tsx
@@ -1,22 +1,34 @@
 import { Box } from '@mui/material';
 import Head from 'next/head';
-import React, { type PropsWithChildren } from 'react';
+import React, { type PropsWithChildren, useState } from 'react';
 
 import { MainAppBar } from '#/components';
+import { useBinaryTree } from '#/hooks';
+import type { BinaryTreeInput } from '#/hooks/useBinaryTree';
 import { SplitPanelsLayout } from '#/layouts';
+import {
+  CodePanel,
+  OutputPanel,
+  SettingsPanel,
+  TreeViewPanel,
+} from '#/layouts/panels';
 import type { GetLayout, NextPageWithLayout } from '#/types/page';
 
 const PlaygroundPage: NextPageWithLayout = () => {
+  const [parsedInput, setParsedInput] = useState<BinaryTreeInput | undefined>();
+
+  const tree = useBinaryTree(parsedInput);
+
   return (
     <>
       <Head>
         <title>LeetPal Playground</title>
       </Head>
       <SplitPanelsLayout>
-        <Box sx={{ p: 1, backgroundColor: 'primary.main' }}>Top Left</Box>
-        <Box sx={{ p: 1, backgroundColor: 'secondary.main' }}>Top Right</Box>
-        <Box sx={{ p: 1, backgroundColor: 'warning.light' }}>Bottom Left</Box>
-        <Box sx={{ p: 1, backgroundColor: 'info.main' }}>Bottom Right</Box>
+        <SettingsPanel setParsedInput={setParsedInput} />
+        <TreeViewPanel tree={tree} />
+        <CodePanel tree={tree} />
+        <OutputPanel />
       </SplitPanelsLayout>
     </>
   );
