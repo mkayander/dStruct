549d59eb23d2d03a0c1fd7e8878e4227b6dd4c9c
mkayander <maxim.kayander1@gmail.com>
2023-01-16 23:39:24 +0100

    feat: Show basic result data at Output panel

--- src/layouts/panels/OutputPanel.tsx
+++ src/layouts/panels/OutputPanel.tsx
@@ -1,16 +1,24 @@
 import { TabContext, TabList } from '@mui/lab';
-import { Box, Tab } from '@mui/material';
+import { alpha, Box, Tab, Typography, useTheme } from '@mui/material';
 import React, { useState } from 'react';
 
 import { PanelWrapper } from '#/layouts/panels/common/PanelWrapper';
 import { StyledTabPanel, TabListWrapper } from '#/layouts/panels/common/styled';
 
+import { useAppSelector } from '#/store/hooks';
+
 export const OutputPanel: React.FC = () => {
+  const theme = useTheme();
+
   const [value, setValue] = useState('1');
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
+
+  const { isReady, runtime, result, error } = useAppSelector(
+    (state) => state.callstack
+  );
 
   return (
     <PanelWrapper>
@@ -18,16 +26,57 @@ export const OutputPanel: React.FC = () => {
         <TabListWrapper>
           <TabList onChange={handleChange} aria-label="panel tabs">
             <Tab label="Output" value="1" />
+            <Tab label="Callstack" value="2" />
           </TabList>
         </TabListWrapper>
-        <Box
-          sx={{
-            p: 1,
-            mt: 1,
-          }}
-        >
-          <StyledTabPanel value="1">Output goes here</StyledTabPanel>
-        </Box>
+
+        <StyledTabPanel value="1">
+          <Box display="flex" flexDirection="row" gap={2}>
+            {!isReady ? (
+              <Box display="box">
+                <Typography variant="h5">No data</Typography>
+                <Typography variant="caption">
+                  You need to run the code first
+                </Typography>
+              </Box>
+            ) : error ? (
+              <Box display="box">
+                <Typography variant="h5" color="error">
+                  {error.name}: {error.message}
+                </Typography>
+                <Typography variant="caption" color="error">
+                  {error.stack}
+                </Typography>
+              </Box>
+            ) : (
+              <Box display="flex" flexDirection="column" gap={1}>
+                <Typography variant="h5" color="success.main">
+                  Success
+                </Typography>
+                <Typography variant="caption">
+                  Returned:{' '}
+                  <Box
+                    component="span"
+                    sx={{
+                      backgroundColor: alpha(theme.palette.success.dark, 0.2),
+                      p: 1,
+                      borderRadius: 2,
+                    }}
+                  >
+                    {result}
+                  </Box>
+                </Typography>
+              </Box>
+            )}
+
+            {runtime && (
+              <Typography variant="caption" color="text.disabled" mt={1}>
+                Runtime: {runtime.toFixed(2)} ms
+              </Typography>
+            )}
+          </Box>
+        </StyledTabPanel>
+        <StyledTabPanel value="2"></StyledTabPanel>
       </TabContext>
     </PanelWrapper>
   );
