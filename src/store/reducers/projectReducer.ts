ee994fe48808972920c2eaea0b02816779c1a501
mkayander <maxim.kayander1@gmail.com>
2023-01-23 23:34:44 +0100

    feat: Updated project reducer

--- src/store/reducers/projectReducer.ts
+++ src/store/reducers/projectReducer.ts
@@ -2,15 +2,24 @@ import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
 import type { RootState } from '#/store/makeStore';
 
+import type { PlaygroundTestCase } from '.prisma/client';
+
+type StringDates<T> = Omit<T, 'createdAt' | 'updatedAt'> & {
+  createdAt: string;
+  updatedAt: string;
+};
+
 type ProjectState = {
   currentProjectId: string | null;
   currentCaseId: string | null;
+  currentCase: StringDates<PlaygroundTestCase> | null;
   currentSolutionId: string | null;
 };
 
 const initialState: ProjectState = {
   currentProjectId: null,
   currentCaseId: null,
+  currentCase: null,
   currentSolutionId: null,
 };
 
@@ -23,6 +32,20 @@ export const projectSlice = createSlice({
       return {
         ...state,
         ...payload,
+      };
+    },
+    setCurrentCase: (
+      state,
+      action: PayloadAction<PlaygroundTestCase | null>
+    ) => {
+      const { payload } = action;
+      return {
+        ...state,
+        currentCase: payload && {
+          ...payload,
+          createdAt: payload.createdAt.toISOString(),
+          updatedAt: payload.updatedAt.toISOString(),
+        },
       };
     },
     clear: () => ({ ...initialState }),
@@ -41,5 +64,7 @@ export const selectCurrentProjectId = (state: RootState) =>
   state.project.currentProjectId;
 export const selectCurrentCaseId = (state: RootState) =>
   state.project.currentCaseId;
+export const selectCurrentCase = (state: RootState) =>
+  state.project.currentCase;
 export const selectCurrentSolutionId = (state: RootState) =>
   state.project.currentSolutionId;
