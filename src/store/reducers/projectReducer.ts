1fabffefd934025e0172371f69d12120b764aa7f
mkayander <maxim.kayander1@gmail.com>
2023-01-27 00:03:03 +0100

    feat: Added isEditable state flag

--- src/store/reducers/projectReducer.ts
+++ src/store/reducers/projectReducer.ts
@@ -3,23 +3,18 @@ import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
 import type { RootState } from '#/store/makeStore';
 
-type StringDates<T> = Omit<T, 'createdAt' | 'updatedAt'> & {
-  createdAt: string;
-  updatedAt: string;
-};
-
 type ProjectState = {
   currentProjectId: string | null;
   currentCaseId: string | null;
-  currentCase: StringDates<PlaygroundTestCase> | null;
   currentSolutionId: string | null;
+  isEditable: boolean;
 };
 
 const initialState: ProjectState = {
   currentProjectId: null,
   currentCaseId: null,
-  currentCase: null,
   currentSolutionId: null,
+  isEditable: false,
 };
 
 export const projectSlice = createSlice({
@@ -63,7 +58,6 @@ export const selectCurrentProjectId = (state: RootState) =>
   state.project.currentProjectId;
 export const selectCurrentCaseId = (state: RootState) =>
   state.project.currentCaseId;
-export const selectCurrentCase = (state: RootState) =>
-  state.project.currentCase;
 export const selectCurrentSolutionId = (state: RootState) =>
   state.project.currentSolutionId;
+export const selectIsEditable = (state: RootState) => state.project.isEditable;
