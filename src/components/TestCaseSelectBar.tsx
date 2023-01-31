f013a86e04d386d463e66ee19b1fa9a5227672f1
mkayander <maxim.kayander1@gmail.com>
2023-01-31 23:20:44 +0100

    feat: Added basic case modal usage

--- src/components/TestCaseSelectBar.tsx
+++ src/components/TestCaseSelectBar.tsx
@@ -2,12 +2,13 @@ import { Add } from "@mui/icons-material";
 import { CircularProgress, IconButton, Stack } from "@mui/material";
 import type { PlaygroundTestCase } from "@prisma/client";
 import type { UseQueryResult } from "@tanstack/react-query";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   SelectBarChip,
   SelectBarChipSkeleton,
 } from "#/components/SelectBarChip";
+import { CaseModal } from "#/layouts/modals";
 import { trpc } from "#/utils";
 import type { RouterOutputs } from "#/utils/trpc";
 
@@ -27,10 +28,12 @@ type TestCaseSelectBarProps = {
 export const TestCaseSelectBar: React.FC<TestCaseSelectBarProps> = ({
   selectedProject,
 }) => {
+  const dispatch = useAppDispatch();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const selectedCaseId = useAppSelector(selectCurrentCaseId);
   const selectedProjectId = selectedProject.data?.id;
-
-  const dispatch = useAppDispatch();
 
   const trpcUtils = trpc.useContext();
   const addCase = trpc.project.addCase.useMutation({
@@ -96,50 +99,58 @@ export const TestCaseSelectBar: React.FC<TestCaseSelectBarProps> = ({
   };
 
   return (
-    <Stack direction="row" flexWrap="wrap" gap={1}>
-      {!selectedProject.data && (
-        <>
-          <SelectBarChipSkeleton width={112} />
-          <SelectBarChipSkeleton width={42} />
-          <SelectBarChipSkeleton />
-          <SelectBarChipSkeleton width={24} />
-        </>
-      )}
+    <>
+      <CaseModal
+        open={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onCaseUpdate={() => {}}
+        onCaseDelete={() => {}}
+      />
+      <Stack direction="row" flexWrap="wrap" gap={1}>
+        {!selectedProject.data && (
+          <>
+            <SelectBarChipSkeleton width={112} />
+            <SelectBarChipSkeleton width={42} />
+            <SelectBarChipSkeleton />
+            <SelectBarChipSkeleton width={24} />
+          </>
+        )}
 
-      {cases?.map((testCase) => {
-        const isCurrent = testCase.id === selectedCaseId;
+        {cases?.map((testCase) => {
+          const isCurrent = testCase.id === selectedCaseId;
 
-        return (
-          <SelectBarChip
-            key={testCase.id}
-            isCurrent={isCurrent}
-            isEditable={isEditable}
-            label={testCase.title}
+          return (
+            <SelectBarChip
+              key={testCase.id}
+              isCurrent={isCurrent}
+              isEditable={isEditable}
+              label={testCase.title}
+              disabled={isLoading}
+              onClick={() => handleCaseClick(testCase)}
+              onDelete={() => {
+                confirm(
+                  `Are you sure you want to delete "${testCase.title}"? This action cannot be undone.`
+                ) && handleCaseDelete(testCase); // TODO: use a modal instead of prompt
+              }}
+            />
+          );
+        })}
+
+        {isEditable && (
+          <IconButton
+            title="Add new test case 🧪"
+            size="small"
+            onClick={handleAddCase}
             disabled={isLoading}
-            onClick={() => handleCaseClick(testCase)}
-            onDelete={() => {
-              confirm(
-                `Are you sure you want to delete "${testCase.title}"? This action cannot be undone.`
-              ) && handleCaseDelete(testCase); // TODO: use a modal instead of prompt
-            }}
-          />
-        );
-      })}
-
-      {isEditable && (
-        <IconButton
-          title="Add new test case 🧪"
-          size="small"
-          onClick={handleAddCase}
-          disabled={isLoading}
-        >
-          {isLoading ? (
-            <CircularProgress size="1.3rem" />
-          ) : (
-            <Add fontSize="small" />
-          )}
-        </IconButton>
-      )}
-    </Stack>
+          >
+            {isLoading ? (
+              <CircularProgress size="1.3rem" />
+            ) : (
+              <Add fontSize="small" />
+            )}
+          </IconButton>
+        )}
+      </Stack>
+    </>
   );
 };
