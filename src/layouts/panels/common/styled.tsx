bb111292edbf24b330cfc53415815204f9606ab6
mkayander <maxim.kayander1@gmail.com>
2023-02-05 00:00:24 +0100

    feat: Implement overlay scrollbars for all panels

--- src/layouts/panels/common/styled.tsx
+++ src/layouts/panels/common/styled.tsx
@@ -1,6 +1,23 @@
-import { TabPanel } from "@mui/lab";
-import { Box } from "@mui/material";
+import { TabPanel, type TabPanelProps } from "@mui/lab";
+import { alpha, Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
+import React from "react";
+
+const StyledOverlayScrollbarsComponent = styled(OverlayScrollbarsComponent)(
+  ({ theme }) => ({
+    ".os-scrollbar > .os-scrollbar-track > div.os-scrollbar-handle": {
+      transition: "background .2s",
+      background: alpha(theme.palette.divider, 0.1),
+      "&:hover": {
+        background: alpha(theme.palette.primary.light, 0.2),
+      },
+      "&:active": {
+        background: alpha(theme.palette.primary.light, 0.5),
+      },
+    },
+  })
+);
 
 export const TabListWrapper = styled(Box)(({ theme }) => ({
   display: "flex",
@@ -16,6 +33,8 @@ export const TabListWrapper = styled(Box)(({ theme }) => ({
   },
 }));
 
-export const StyledTabPanel = styled(TabPanel)(({ theme }) => ({
-  padding: theme.spacing(2),
-}));
+export const StyledTabPanel: React.FC<TabPanelProps> = (props) => (
+  <StyledOverlayScrollbarsComponent defer>
+    <TabPanel {...props} />
+  </StyledOverlayScrollbarsComponent>
+);
