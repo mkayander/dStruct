41a7dc57e5ebebcc628edbca2936233ee74f93fc
mkayander <maxim.kayander1@gmail.com>
2023-02-13 19:30:00 +0100

    feat: Remove unused redux state

--- src/store/reducers/projectReducer.ts
+++ src/store/reducers/projectReducer.ts
@@ -3,16 +3,10 @@ import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "#/store/makeStore";
 
 type ProjectState = {
-  currentProjectId: string | null;
-  currentCaseId: string | null;
-  currentSolutionId: string | null;
   isEditable: boolean;
 };
 
 const initialState: ProjectState = {
-  currentProjectId: null,
-  currentCaseId: null,
-  currentSolutionId: null,
   isEditable: false,
 };
 
@@ -52,10 +46,4 @@ export const projectReducer = projectSlice.reducer;
 /**
  * Selectors
  */
-export const selectCurrentProjectId = (state: RootState) =>
-  state.project.currentProjectId;
-export const selectCurrentCaseId = (state: RootState) =>
-  state.project.currentCaseId;
-export const selectCurrentSolutionId = (state: RootState) =>
-  state.project.currentSolutionId;
 export const selectIsEditable = (state: RootState) => state.project.isEditable;
