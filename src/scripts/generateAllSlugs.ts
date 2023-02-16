101e7398c2c172b4c9be0134554101356ad7d974
mkayander <maxim.kayander1@gmail.com>
2023-02-16 23:48:29 +0100

    feat: Implemented slug generation script

---
+++ src/scripts/generateAllSlugs.ts
@@ -0,0 +1,43 @@
+import shortUUID from "short-uuid";
+
+import { prisma } from "#/server/db/client";
+
+const uuid = shortUUID();
+
+(async () => {
+  const projects = await prisma.playgroundProject.findMany();
+  await prisma.$transaction(
+    projects.map((item) =>
+      prisma.playgroundProject.update({
+        where: { id: item.id },
+        data: {
+          slug: `project-${uuid.generate()}`,
+        },
+      })
+    )
+  );
+
+  const cases = await prisma.playgroundTestCase.findMany();
+  await prisma.$transaction(
+    cases.map((item) =>
+      prisma.playgroundTestCase.update({
+        where: { id: item.id },
+        data: {
+          slug: `case-${uuid.generate()}`,
+        },
+      })
+    )
+  );
+
+  const solutions = await prisma.playgroundSolution.findMany();
+  await prisma.$transaction(
+    solutions.map((item) =>
+      prisma.playgroundSolution.update({
+        where: { id: item.id },
+        data: {
+          slug: `solution-${uuid.generate()}`,
+        },
+      })
+    )
+  );
+})();
