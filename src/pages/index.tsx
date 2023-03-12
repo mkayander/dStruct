a7b15043cd855802ffa31398b76a82fc16e94adc
mkayander <maxim.kayander1@gmail.com>
2023-03-12 22:58:52 +0100

    fix: Test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,12 @@ import React from "react";
 import { useDailyQuestionData } from "#/api";
 import { QuestionSummary } from "#/components";
 import { useGetUserProfileQuery } from "#/graphql/generated";
-import { DailyProblem, LeetCodeStats, UserSettings } from "#/layouts";
+import {
+  DailyProblem,
+  LeetCodeStats,
+  MainLayout,
+  UserSettings,
+} from "#/layouts";
 
 const DashboardPage: React.FC = () => {
   const session = useSession();
@@ -27,45 +32,48 @@ const DashboardPage: React.FC = () => {
   const questionDataQuery = useDailyQuestionData();
 
   return (
-    <Container>
-      <Typography variant="h5" my={3}>
-        {leetCodeUsername ? `${leetCodeUsername}'s Dashboard` : "Dashboard"}
-      </Typography>
-      <Grid container spacing={2}>
-        {session.status === "loading" ? (
-          <CircularProgress />
-        ) : session.status === "authenticated" ? (
-          <>
-            <Grid item xs={12} md={6}>
-              <UserSettings />
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    <MainLayout setIsLightMode={() => {}}>
+      <Container>
+        <Typography variant="h5" my={3}>
+          {leetCodeUsername ? `${leetCodeUsername}'s Dashboard` : "Dashboard"}
+        </Typography>
+        <Grid container spacing={2}>
+          {session.status === "loading" ? (
+            <CircularProgress />
+          ) : session.status === "authenticated" ? (
+            <>
+              <Grid item xs={12} md={6}>
+                <UserSettings />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <LeetCodeStats userProfile={userProfileQueryResult} />
+              </Grid>
+            </>
+          ) : (
+            <Grid item xs={12}>
+              <Tooltip title="Sign in with GitHub or Google at the top right corner">
+                <Typography
+                  variant="h4"
+                  sx={{
+                    width: "fit-content",
+                    margin: "auto",
+                  }}
+                >
+                  Please sign in to start using the benefits! 🚀
+                </Typography>
+              </Tooltip>
             </Grid>
-            <Grid item xs={12} md={6}>
-              <LeetCodeStats userProfile={userProfileQueryResult} />
-            </Grid>
-          </>
-        ) : (
+          )}
           <Grid item xs={12}>
-            <Tooltip title="Sign in with GitHub or Google at the top right corner">
-              <Typography
-                variant="h4"
-                sx={{
-                  width: "fit-content",
-                  margin: "auto",
-                }}
-              >
-                Please sign in to start using the benefits! 🚀
-              </Typography>
-            </Tooltip>
+            <QuestionSummary questionDataQuery={questionDataQuery} my={24} />
           </Grid>
-        )}
-        <Grid item xs={12}>
-          <QuestionSummary questionDataQuery={questionDataQuery} my={24} />
+          <Grid item>
+            <DailyProblem questionDataQuery={questionDataQuery} />
+          </Grid>
         </Grid>
-        <Grid item>
-          <DailyProblem questionDataQuery={questionDataQuery} />
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </MainLayout>
   );
 };
 
