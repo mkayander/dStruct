a1b36e9519469f76aeb49beb21e6660403454f0d
mkayander <maxim.kayander1@gmail.com>
2023-03-18 18:48:44 +0100

    fix: Avoid blocking route history (fixes #29)

--- src/hooks/usePlaygroundSlugs.ts
+++ src/hooks/usePlaygroundSlugs.ts
@@ -13,13 +13,13 @@ export const usePlaygroundSlugs = () => {
       ? router.query.slug
       : [];
 
-    const setProject = (slug: string) => router.push(`${BASE_PATH}/${slug}`);
+    const setProject = (slug: string) => router.replace(`${BASE_PATH}/${slug}`);
     const setCase = (slug: string) => {
       if (!projectSlug) throw new Error("Project id must be set first");
 
       if (!slug) return setProject(projectSlug);
 
-      return router.push(
+      return router[caseSlug ? "push" : "replace"](
         `${BASE_PATH}/${projectSlug}/${slug}/${solutionSlug ?? ""}`
       );
     };
@@ -27,7 +27,9 @@ export const usePlaygroundSlugs = () => {
       if (!projectSlug || !caseSlug)
         throw new Error("Project and case ids must be set first");
 
-      return router.push(`${BASE_PATH}/${projectSlug}/${caseSlug}/${slug}`);
+      return router[solutionSlug ? "push" : "replace"](
+        `${BASE_PATH}/${projectSlug}/${caseSlug}/${slug}`
+      );
     };
 
     const clearSlugs = () => {
