f51dd1e775d8927eb4c60786b84d269e5b308d8e
mkayander <maxim.kayander1@gmail.com>
2023-03-23 23:26:38 +0100

    feat: Implemented boolean toggle input

---
+++ src/components/ArgsEditor/BooleanToggleInput.tsx
@@ -0,0 +1,34 @@
+import { FormControlLabel, Switch, type SwitchProps } from "@mui/material";
+import React from "react";
+
+type BooleanToggleInputProps = Omit<SwitchProps, "value" | "onChange"> & {
+  value: string;
+  onChange(value: string): void;
+};
+
+export const BooleanToggleInput: React.FC<BooleanToggleInputProps> = ({
+  value,
+  onChange,
+  ...restProps
+}) => {
+  const handleChange: React.EventHandler<
+    React.ChangeEvent<HTMLInputElement>
+  > = (ev) => {
+    onChange(ev.target.checked ? "true" : "false");
+  };
+
+  return (
+    <FormControlLabel
+      control={
+        <Switch
+          checked={value === "true"}
+          onChange={handleChange}
+          {...restProps}
+        />
+      }
+      label={value === "true" ? "True" : "False"}
+      labelPlacement="end"
+      sx={{ ml: 1 }}
+    />
+  );
+};
