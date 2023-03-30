7f2f2875ea6f7ac71cc94b4df9febb22083256dc
mkayander <maxim.kayander1@gmail.com>
2023-03-30 23:05:47 +0200

    feat: Added linked list project category

--- src/utils/categoryLabels.ts
+++ src/utils/categoryLabels.ts
@@ -2,6 +2,7 @@ import { ProjectCategory } from "@prisma/client";
 
 export const categoryLabels: Record<ProjectCategory, string> = {
   [ProjectCategory.BINARY_TREE]: "Binary Tree",
+  [ProjectCategory.LINKED_LIST]: "Linked List",
   [ProjectCategory.ARRAY]: "Array",
   [ProjectCategory.BST]: "BST",
   [ProjectCategory.GRAPH]: "Graph",
