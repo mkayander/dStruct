d46656964d749457f9d1cb58d9ad7c4faaf66a80
mkayander <maxim.kayander1@gmail.com>
2023-04-05 23:02:30 +0200

    fix: Prevent wrong warnings

--- src/utils/validateAnimationName.ts
+++ src/utils/validateAnimationName.ts
@@ -3,11 +3,13 @@ import type { AnimationName } from "#/store/reducers/structures/treeNodeReducer"
 export const validateAnimationName = (
   name?: string
 ): AnimationName | undefined => {
+  if (!name) return;
+
   switch (name) {
     case "blink":
       return name;
     default:
       console.warn(`Invalid animation name: ${name}`);
-      return undefined;
+      return;
   }
 };
