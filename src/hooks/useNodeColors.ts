71085f8662d8608f4eab403a49fd86081fd89d6b
mkayander <maxim.kayander1@gmail.com>
2023-04-05 23:03:17 +0200

    feat: Updated node colors hook

--- src/hooks/useNodeColors.ts
+++ src/hooks/useNodeColors.ts
@@ -1,17 +1,25 @@
 import { useTheme } from "@mui/material";
 import * as muiColors from "@mui/material/colors";
 
-export const useNodeColors = (color?: string) => {
+export const useNodeColors = (color?: string, useDefaultPrimary = true) => {
   const theme = useTheme();
 
-  let nodeColor = theme.palette.primary.main;
-  let shadowColor = theme.palette.primary.dark;
+  let nodeColor: string | undefined = undefined;
+  let shadowColor: string | undefined = undefined;
+  if (useDefaultPrimary) {
+    nodeColor = theme.palette.primary.main;
+    shadowColor = theme.palette.primary.dark;
+  }
   type ColorName = keyof typeof muiColors;
   if (color && color in muiColors) {
     const colorMap = muiColors[color as ColorName];
     if ("500" in colorMap) {
       nodeColor = shadowColor = colorMap[500];
     }
+  } else if (color === "white") {
+    nodeColor = shadowColor = "#fff";
+  } else if (color === "black") {
+    nodeColor = shadowColor = "#000";
   }
 
   return { nodeColor, shadowColor };
