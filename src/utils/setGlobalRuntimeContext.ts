6958e8e1ff845cb8b2e475c7143594528c467a81
mkayander <maxim.kayander1@gmail.com>
2023-04-21 21:32:56 +0200

    feat: Implemented ArrayProxy runtime interface for solutions

--- src/utils/setGlobalRuntimeContext.ts
+++ src/utils/setGlobalRuntimeContext.ts
@@ -1,9 +1,11 @@
 import { Queue } from "@datastructures-js/queue";
 import shortUUID from "short-uuid";
 
+import { ControlledArray } from "#/hooks/dataStructures/arrayStructure";
 import { BinaryTreeNode } from "#/hooks/dataStructures/binaryTreeNode";
 import { LinkedListNode } from "#/hooks/dataStructures/linkedListNode";
 import { type AppDispatch } from "#/store/makeStore";
+import { generateArrayData } from "#/store/reducers/structures/arrayReducer";
 import { ArgumentType } from "#/utils/argumentObject";
 
 const uuid = shortUUID();
@@ -24,7 +26,7 @@ export const setGlobalRuntimeContext = (dispatch: AppDispatch) => {
           type: ArgumentType.BINARY_TREE,
           depth: 0,
         },
-        "candidate",
+        uuid.generate(),
         dispatch
       );
     }
@@ -39,14 +41,38 @@ export const setGlobalRuntimeContext = (dispatch: AppDispatch) => {
           id: uuid.generate(),
           type: ArgumentType.LINKED_LIST,
         },
-        "candidate",
+        uuid.generate(),
         dispatch,
         true
       );
     }
   }
 
+  class ArrayProxy<T extends string | number> extends ControlledArray<T> {
+    constructor(arrayLength: number);
+    constructor(...items: Array<T>) {
+      if (items.length === 1 && typeof items[0] === "number") {
+        const arrayLength = items[0];
+        items = new Array(arrayLength);
+      }
+
+      if (typeof items[0] !== "number" && typeof items[0] !== "string") {
+        throw new Error("ArrayProxy can only contain numbers or strings");
+      }
+
+      const data = generateArrayData(items);
+
+      super(items, uuid.generate(), data, dispatch, true);
+    }
+
+    static override from(array: Array<number | string>) {
+      const data = generateArrayData(array);
+      return new ControlledArray(array, uuid.generate(), data, dispatch, true);
+    }
+  }
+
   const context = {
+    ArrayProxy,
     Queue,
     BinaryTree,
     LinkedList,
