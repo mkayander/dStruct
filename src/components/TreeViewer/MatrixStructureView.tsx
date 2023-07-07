1948f1e86e6b5840dd8d2761be0e33f668d42bb9
mkayander <maxim.kayander1@gmail.com>
2023-07-07 23:41:23 +0200

    feat: Implemented Matrix structure view component

---
+++ src/components/TreeViewer/MatrixStructureView.tsx
@@ -0,0 +1,36 @@
+import { alpha, Stack, useTheme } from "@mui/material";
+
+import { ArrayStructureView } from "#/components/TreeViewer/ArrayStructureView";
+import {
+  type ArrayData,
+  type ArrayDataState,
+} from "#/store/reducers/structures/arrayReducer";
+
+type MatrixStructureViewProps = {
+  data: ArrayData;
+  arrayState: ArrayDataState;
+};
+
+export const MatrixStructureView: React.FC<MatrixStructureViewProps> = ({
+  data,
+  arrayState,
+}) => {
+  const theme = useTheme();
+
+  return (
+    <Stack
+      sx={{
+        "& > *:not(:last-of-type)": {
+          borderBottom: `1px solid ${alpha(theme.palette.primary.light, 0.3)}}`,
+        },
+      }}
+    >
+      {data.childNames?.map((name) => {
+        const data = arrayState[name];
+        if (!data) return null;
+
+        return <ArrayStructureView key={name} data={data} isGrid={true} />;
+      })}
+    </Stack>
+  );
+};
