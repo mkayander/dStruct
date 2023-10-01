3c087f3e9343ad28a5cfef8a00bdc799e49a31ad
mkayander <maxim.kayander1@gmail.com>
2023-10-01 18:52:28 +0200

    feat: Improved app bar layout

--- src/components/organisms/MainAppBar.tsx
+++ src/components/organisms/MainAppBar.tsx
@@ -124,192 +124,193 @@ export const MainAppBar: React.FC<MainAppBarProps> = ({
   };
 
   return (
-    <AppBar
-      position="sticky"
-      elevation={isScrolled ? 2 : 0}
-      variant={appBarVariant}
-      sx={{
-        transition: "background .1s, borderBottom .1s",
-        backdropFilter: "blur(12px)",
-        boxShadow: `0 0 12px 0 ${alpha(
-          theme.palette.primary.main,
-          isScrolled ? 0.2 : 0,
-        )}`,
-        background: isScrolled
-          ? alpha(theme.palette.primary.main, 0.2)
-          : alpha(theme.palette.primary.main, 0),
-        borderBottom: `1px solid ${alpha(
-          theme.palette.common.white,
-          isScrolled ? 0.05 : 0,
-        )}`,
-        color: theme.palette.text.primary,
-      }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters variant={toolbarVariant}>
-          <Box sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}>
-            <Image
-              alt={LL.DSTRUCT_LOGO()}
-              src="/android-chrome-192x192.png"
-              width="32"
-              height="32"
-            />
-          </Box>
-          {/* TODO: Remove legacyBehavior - issue with nested MUI links https://github.com/mui/material-ui/issues/34898 */}
-          <Link href="/" legacyBehavior={true}>
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "'Share Tech', sans-serif",
-                textDecoration: "none",
-              }}
-            >
-              dStruct
-            </Typography>
-          </Link>
+    <>
+      <SidePanel isOpen={isSidePanelOpen} setIsOpen={setIsSidePanelOpen} />
+      <AppBar
+        position="sticky"
+        elevation={isScrolled ? 2 : 0}
+        variant={appBarVariant}
+        sx={{
+          transition: "background .1s, borderBottom .1s",
+          backdropFilter: "blur(12px)",
+          boxShadow: `0 0 12px 0 ${alpha(
+            theme.palette.primary.main,
+            isScrolled ? 0.2 : 0,
+          )}`,
+          background: isScrolled
+            ? alpha(theme.palette.primary.main, 0.2)
+            : alpha(theme.palette.primary.main, 0),
+          borderBottom: `1px solid ${alpha(
+            theme.palette.common.white,
+            isScrolled ? 0.05 : 0,
+          )}`,
+          color: theme.palette.text.primary,
+        }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar disableGutters variant={toolbarVariant} sx={{ height: 56 }}>
+            <Box sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}>
+              <Image
+                alt={LL.DSTRUCT_LOGO()}
+                src="/android-chrome-192x192.png"
+                width="32"
+                height="32"
+              />
+            </Box>
+            {/* TODO: Remove legacyBehavior - issue with nested MUI links https://github.com/mui/material-ui/issues/34898 */}
+            <Link href="/" legacyBehavior={true}>
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "'Share Tech', sans-serif",
+                  textDecoration: "none",
+                }}
+              >
+                dStruct
+              </Typography>
+            </Link>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label={LL.CURRENT_USER_ACCOUNT()}
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label={LL.CURRENT_USER_ACCOUNT()}
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem
+                    key={page.href}
+                    onClick={() => handleNavItemClick(page)}
+                  >
+                    <Typography textAlign="center">{page.name}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Box sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}>
+              <Image
+                alt={LL.DSTRUCT_LOGO()}
+                src="/android-chrome-192x192.png"
+                width="32"
+                height="32"
+              />
+            </Box>
+            <Link href="/" legacyBehavior={true}>
+              <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "'Share Tech', sans-serif",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                dStruct
+              </Typography>
+            </Link>
+            <Box
+              sx={{ flexGrow: 1, gap: 1, display: { xs: "none", md: "flex" } }}
             >
               {pages.map((page) => (
-                <MenuItem
+                <Button
                   key={page.href}
                   onClick={() => handleNavItemClick(page)}
+                  color="inherit"
                 >
-                  <Typography textAlign="center">{page.name}</Typography>
-                </MenuItem>
+                  {page.name}
+                </Button>
               ))}
-            </Menu>
-          </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}>
-            <Image
-              alt={LL.DSTRUCT_LOGO()}
-              src="/android-chrome-192x192.png"
-              width="32"
-              height="32"
-            />
-          </Box>
-          <Link href="/" legacyBehavior={true}>
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "'Share Tech', sans-serif",
-                color: "inherit",
-                textDecoration: "none",
-              }}
+            </Box>
+
+            <Stack
+              direction="row"
+              spacing={1}
+              alignItems="center"
+              sx={{ flexGrow: 0 }}
             >
-              dStruct
-            </Typography>
-          </Link>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page.href}
-                onClick={() => handleNavItemClick(page)}
-                color="inherit"
-                sx={{ my: 2, display: "block" }}
-              >
-                {page.name}
-              </Button>
-            ))}
-          </Box>
-
-          <Stack
-            direction="row"
-            spacing={1}
-            alignItems="center"
-            sx={{ flexGrow: 0 }}
-          >
-            <ThemeSwitch
-              onChange={(_, checked) => {
-                handleLightModeSwitch(!checked);
-              }}
-            />
-
-            {session.status === "loading" ? (
-              <Skeleton
-                variant="circular"
-                animation="wave"
-                height={40}
-                width={40}
+              <ThemeSwitch
+                onChange={(_, checked) => {
+                  handleLightModeSwitch(!checked);
+                }}
               />
-            ) : (
-              <>
-                {session.status === "unauthenticated" ? (
-                  <Button
-                    title={LL.SIGN_IN()}
-                    color="inherit"
-                    onClick={handleSignIn}
-                  >
-                    {LL.SIGN_IN()}
-                  </Button>
-                ) : null}
-                <Tooltip title={LL.OPEN_OPTIONS()} arrow>
-                  <IconButton onClick={handleOpenUserMenu}>
-                    {session.data ? (
-                      <Avatar>
-                        <Image
-                          src={getImageUrl(
-                            session.data.user.bucketImage || AVATAR_PLACEHOLDER,
-                          )}
-                          alt={`${session.data.user.name} avatar`}
-                          referrerPolicy="no-referrer"
-                          crossOrigin="anonymous"
-                          fill={true}
-                        />
-                      </Avatar>
-                    ) : (
-                      <Settings />
-                    )}
-                  </IconButton>
-                </Tooltip>
-              </>
-            )}
 
-            <SidePanel
-              isOpen={isSidePanelOpen}
-              setIsOpen={setIsSidePanelOpen}
-            />
-          </Stack>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              {session.status === "loading" ? (
+                <Skeleton
+                  variant="circular"
+                  animation="wave"
+                  height={40}
+                  width={40}
+                />
+              ) : (
+                <>
+                  {session.status === "unauthenticated" ? (
+                    <Button
+                      title={LL.SIGN_IN()}
+                      color="inherit"
+                      onClick={handleSignIn}
+                    >
+                      {LL.SIGN_IN()}
+                    </Button>
+                  ) : null}
+                  <Tooltip title={LL.OPEN_OPTIONS()} arrow>
+                    <IconButton onClick={handleOpenUserMenu}>
+                      {session.data ? (
+                        <Avatar>
+                          <Image
+                            src={getImageUrl(
+                              session.data.user.bucketImage ||
+                                AVATAR_PLACEHOLDER,
+                            )}
+                            alt={`${session.data.user.name} avatar`}
+                            referrerPolicy="no-referrer"
+                            crossOrigin="anonymous"
+                            width={40}
+                            height={40}
+                          />
+                        </Avatar>
+                      ) : (
+                        <Settings />
+                      )}
+                    </IconButton>
+                  </Tooltip>
+                </>
+              )}
+            </Stack>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </>
   );
 };
