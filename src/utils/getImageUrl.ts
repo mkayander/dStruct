e66476934ea811783675c23600d8d881c7834bf7
mkayander <maxim.kayander1@gmail.com>
2023-10-01 21:07:12 +0200

    fix: Use client process env directly

--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -1,4 +1,2 @@
-import { clientEnv as env } from "#/env/schema.mjs";
-
 export const getImageUrl = (relativePath: string) =>
-  `${env.NEXT_PUBLIC_BUCKET_BASE_URL}/${relativePath}`;
+  `${process.env.NEXT_PUBLIC_BUCKET_BASE_URL}/${relativePath}`;
