594e64bebf7aa0b156c1fa2cf7ba45f5113672fc
mkayander <maxim.kayander1@gmail.com>
2023-10-01 21:29:19 +0200

    feat: Mock env variables for testing

--- jest.setup.mjs
+++ jest.setup.mjs
@@ -3,3 +3,6 @@ import "@testing-library/jest-dom";
 import { TextDecoder, TextEncoder } from "util";
 
 Object.assign(global, { TextDecoder, TextEncoder });
+
+// Mock env variables
+process.env.NEXT_PUBLIC_BUCKET_BASE_URL = "https://leetpal.s3.amazonaws.com";
