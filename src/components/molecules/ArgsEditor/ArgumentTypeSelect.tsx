6ef2427d3d3296b24a6e259d63c8353dc073abbf
mkayander <maxim.kayander1@gmail.com>
2023-10-17 00:25:35 +0300

    fix: Fixed dropdown text selector

--- src/components/molecules/ArgsEditor/ArgumentTypeSelect.tsx
+++ src/components/molecules/ArgsEditor/ArgumentTypeSelect.tsx
@@ -64,7 +64,7 @@ export const ArgumentTypeSelect: React.FC<ArgumentTypeSelectProps> = ({
         size="small"
         sx={{
           width: "70px",
-          "& [role=button]": {
+          "& [role=combobox]": {
             color: "transparent",
             "& > svg": {
               ml: 0.5,
