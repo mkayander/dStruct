9f52473c27a2f1a1b085906aafacd4083146285e
mkayander <maxim.kayander1@gmail.com>
2023-11-11 19:12:11 +0300

    feat: Implemented a matrix row component

---
+++ src/components/molecules/TreeViewer/MatrixRow.tsx
@@ -0,0 +1,32 @@
+import { Box } from "@mui/material";
+import React, { useMemo } from "react";
+
+import { ArrayItem } from "#/components/molecules/TreeViewer/ArrayItem";
+import { type ArrayStructureViewProps } from "#/components/molecules/TreeViewer/ArrayStructureView";
+import { arrayDataItemSelectors } from "#/store/reducers/structures/arrayReducer";
+
+export const MatrixRow: React.FC<ArrayStructureViewProps> = ({ data }) => {
+  const items = useMemo(
+    () => arrayDataItemSelectors.selectAll(data.nodes),
+    [data.nodes],
+  );
+
+  return (
+    <tr>
+      {items.length > 0 ? (
+        items.map((item) => (
+          <td key={item.id}>
+            <ArrayItem colorMap={data.colorMap} isGrid={true} {...item} />
+          </td>
+        ))
+      ) : (
+        <Box
+          sx={{
+            width: 44,
+            height: 44,
+          }}
+        />
+      )}
+    </tr>
+  );
+};
