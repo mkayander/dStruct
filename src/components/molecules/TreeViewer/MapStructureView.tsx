29d1edeb70c5e0cfcb4cb4db5a5235288f51dad7
mkayander <maxim.kayander1@gmail.com>
2024-01-08 23:59:39 +0300

    feat: Added map structure view component

---
+++ src/components/molecules/TreeViewer/MapStructureView.tsx
@@ -0,0 +1,44 @@
+import { alpha, Box, useTheme } from "@mui/material";
+import React from "react";
+
+import { MatrixRow } from "#/components/molecules/TreeViewer/MatrixRow";
+import {
+  type ArrayData,
+  type ArrayDataState,
+} from "#/store/reducers/structures/arrayReducer";
+
+type MatrixStructureViewProps = {
+  data: ArrayData;
+  arrayState: ArrayDataState;
+};
+
+export const MapStructureView: React.FC<MatrixStructureViewProps> = ({
+  data,
+  arrayState,
+}) => {
+  const theme = useTheme();
+  const borderColor = `1px solid ${alpha(theme.palette.primary.light, 0.3)}`;
+
+  return (
+    <Box
+      component="table"
+      sx={{
+        width: "fit-content",
+        borderCollapse: "collapse",
+        border: borderColor,
+        td: {
+          border: borderColor,
+        },
+      }}
+    >
+      <tbody>
+        {data.childNames?.map((name) => {
+          const data = arrayState[name];
+          if (!data) return null;
+
+          return <MatrixRow key={name} data={data} />;
+        })}
+      </tbody>
+    </Box>
+  );
+};
