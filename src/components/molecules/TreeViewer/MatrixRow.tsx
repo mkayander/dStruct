76d7ce9bc6d67ebf8eb7575ad49f5823ecda8e4a
mkayander <maxim.kayander1@gmail.com>
2024-01-12 20:23:57 +0300

    feat: Improved matrix structure appearance

--- src/components/molecules/TreeViewer/MatrixRow.tsx
+++ src/components/molecules/TreeViewer/MatrixRow.tsx
@@ -16,7 +16,12 @@ export const MatrixRow: React.FC<ArrayStructureViewProps> = ({ data }) => {
       {items.length > 0 ? (
         items.map((item) => (
           <td key={item.id}>
-            <ArrayItem colorMap={data.colorMap} isGrid={true} item={item} />
+            <ArrayItem
+              size={40}
+              colorMap={data.colorMap}
+              isGrid={true}
+              item={item}
+            />
           </td>
         ))
       ) : (
