1b8a2e26744cbab14002c263b6e6bba83b4fa355
mkayander <maxim.kayander1@gmail.com>
2024-01-21 16:58:54 +0300

    feat: Run worker server

--- workers/main.mjs
+++ workers/main.mjs
@@ -1,28 +1,53 @@
+import http from "http";
 import { spawn } from "node:child_process";
 import path from "path";
 
-const codeStr = `
-x = 2
-y = x ** 8
-print(y)
-`;
+const PORT = 8333;
 
-const child = spawn(`python`, [
-  path.join(process.cwd(), "workers", "exec.py"),
-  codeStr,
-]);
-child.stdout.setEncoding("utf-8");
-child.stdout.on("data", (data) => {
-  console.log("stdout: ", data.toString());
-});
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    console.log(req.headers);
+    console.log(req.method);
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      console.log(body);
+      const data = JSON.parse(body);
+      console.log(data);
 
-child.on("spawn", (...args) => {
-  console.log("Spawn: ", ...args);
-});
-child.on("message", (...args) => console.log(...args));
-child.on("error", (...args) => {
-  console.error("Proc Error: ", ...args);
-});
-child.on("exit", (...args) => {
-  console.log("Exit: ", ...args);
-});
+      const child = spawn(`python`, [
+        path.join(process.cwd(), "workers", "exec.py"),
+        data.code,
+      ]);
+      child.stdout.setEncoding("utf-8");
+
+      let result = "";
+      child.stdout.on("data", (data) => {
+        const chunk = data.toString();
+        result += chunk;
+        console.log("stdout: ", chunk);
+      });
+
+      child.on("error", (...args) => {
+        console.error("Proc Error: ", ...args);
+        res.end();
+      });
+      child.on("exit", (...args) => {
+        console.log("Exit: ", ...args);
+        res.write(
+          JSON.stringify({
+            result,
+            url: req.url,
+            data,
+          }),
+        );
+        res.end();
+      });
+    });
+  })
+  .listen(PORT);
+
+console.log(`Server is running on port ${PORT}`);
