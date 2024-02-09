016dc27049abc5be848a0ae01117da81063e00a8
mkayander <maxim.kayander1@gmail.com>
2024-02-09 17:08:00 +0300

    feat: Implemented separate worker interface functions

---
+++ src/workers/codeExecWorkerInterface.ts
@@ -0,0 +1,43 @@
+import type {
+  ExecWorkerInterface,
+  WorkerRequestType,
+  WorkerResponse,
+} from "#/workers/codeExec.worker";
+
+export const awaitWorkerResponse = <T extends WorkerRequestType>(
+  worker: Worker,
+  type: T,
+  timeLimit = 60000,
+): Promise<ExecWorkerInterface[T]["response"]> =>
+  new Promise((resolve, reject) => {
+    const listener = (event: MessageEvent<WorkerResponse>) => {
+      if (event.data?.type !== type) return;
+
+      console.log("Worker response:", event.data);
+      if (event.data.error) {
+        reject(event.data.error);
+      } else {
+        resolve(event.data);
+      }
+    };
+    worker.addEventListener("message", listener, { once: true });
+
+    setTimeout(() => {
+      worker.removeEventListener("message", listener);
+      reject(
+        new Error(
+          `Worker request time limit expired! No response in ${timeLimit}ms`,
+        ),
+      );
+    }, timeLimit);
+  });
+
+export const requestWorkerAction = async <T extends WorkerRequestType>(
+  worker: Worker,
+  type: T,
+  request: ExecWorkerInterface[T]["request"],
+): Promise<ExecWorkerInterface[T]["response"]> => {
+  const promise = awaitWorkerResponse(worker, type);
+  worker.postMessage(request);
+  return promise;
+};
