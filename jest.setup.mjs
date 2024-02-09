6cba6a4abac60d57b81b8e289e91ff74de01471a
mkayander <maxim.kayander1@gmail.com>
2024-02-09 14:13:14 +0300

    fix: Fix Jest tests by providing missing mocks

--- jest.setup.mjs
+++ jest.setup.mjs
@@ -1,8 +1,10 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import "jest-canvas-mock";
+import ResizeObserver from "resize-observer-polyfill";
 import { TextDecoder, TextEncoder } from "util";
 
-Object.assign(global, { TextDecoder, TextEncoder });
+Object.assign(global, { TextDecoder, TextEncoder, ResizeObserver });
 
 // Mock env variables
 process.env.NEXT_PUBLIC_BUCKET_BASE_URL = "https://leetpal.s3.amazonaws.com";
