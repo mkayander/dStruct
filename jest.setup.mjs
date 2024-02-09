cd439cef78f864d7f886d0dac0268f0c254cd3dc
mkayander <maxim.kayander1@gmail.com>
2024-02-09 17:10:00 +0300

    feat: Setup jsdom-worker

--- jest.setup.mjs
+++ jest.setup.mjs
@@ -1,6 +1,7 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 import "jest-canvas-mock";
+import "jsdom-worker";
 import ResizeObserver from "resize-observer-polyfill";
 import { TextDecoder, TextEncoder } from "util";
 
