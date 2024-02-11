c0bc8ec0c3fcef9236782d1941df7ae30432f8f3
mkayander <maxim.kayander1@gmail.com>
2024-02-11 21:12:39 +0300

    feat: Support basic Box props

--- src/components/templates/MainLayout.tsx
+++ src/components/templates/MainLayout.tsx
@@ -1,11 +1,11 @@
-import { Box, LinearProgress } from "@mui/material";
+import { Box, type BoxProps, LinearProgress } from "@mui/material";
 import React from "react";
 
 import { Footer } from "#/components/organisms/Footer";
 import { MainAppBar } from "#/components/organisms/MainAppBar";
 import { PageScrollContainer } from "#/components/templates/PageScrollContainer";
 
-export type MainLayoutProps = {
+export type MainLayoutProps = BoxProps & {
   children: React.ReactNode;
   isLoading?: boolean;
 };
@@ -13,10 +13,11 @@ export type MainLayoutProps = {
 export const MainLayout: React.FC<MainLayoutProps> = ({
   children,
   isLoading,
+  ...restProps
 }) => {
   return (
     <PageScrollContainer isPage={true} style={{ height: "100vh" }}>
-      <Box sx={{ minHeight: "100vh" }}>
+      <Box sx={{ minHeight: "100vh" }} {...restProps}>
         <MainAppBar />
         <Box component="main" sx={{ minHeight: "85vh" }}>
           {isLoading ? <LinearProgress variant="indeterminate" /> : children}
