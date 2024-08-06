1b7289670f74d51091690d962878bbed014285ec
mkayander <maxim.kayander1@gmail.com>
2024-08-06 15:20:53 +0300

    feat: Added new utils unit tests

--- src/utils/__tests__/index.test.ts
+++ src/utils/__tests__/index.test.ts
@@ -1,9 +1,19 @@
 import { alpha } from "@mui/material";
 import type { RelationType } from "react-archer/lib/types";
 
+import { BinaryTreeNode } from "#/hooks/dataStructures/binaryTreeNode";
+import { LinkedListNode } from "#/hooks/dataStructures/linkedListNode";
+import { CallstackHelper } from "#/store/reducers/callstackReducer";
 import { type TreeNodeData } from "#/store/reducers/structures/treeNodeReducer";
-import { getImageUrl, isNumber, processNodeRelation } from "#/utils";
+import {
+  getImageUrl,
+  isNumber,
+  processNodeRelation,
+  stringifySolutionResult,
+} from "#/utils";
 import { ArgumentType } from "#/utils/argumentObject";
+
+import { safeStringify } from "../stringifySolutionResult";
 
 describe("utils", () => {
   describe("getImageUrl", () => {
@@ -81,6 +91,110 @@ describe("utils", () => {
           style: { strokeColor: alpha(nodeColor, 0.4) },
         },
       ]);
+    });
+  });
+
+  describe("stringifySolutionResult", () => {
+    it('should return "null" for null input', () => {
+      const result = stringifySolutionResult(null);
+      expect(result).toBe("null");
+    });
+
+    it('should return "undefined" for undefined input', () => {
+      const result = stringifySolutionResult(undefined);
+      expect(result).toBe("undefined");
+    });
+  });
+
+  describe("safeStringify", () => {
+    const callstack = new CallstackHelper();
+
+    it("should return the string itself for string input", () => {
+      const result = safeStringify("test");
+      expect(result).toBe("test");
+    });
+
+    it("should return the number itself for number input", () => {
+      const result = safeStringify(123);
+      expect(result).toBe("123");
+    });
+
+    it("should return the bigint itself for bigint input", () => {
+      const result = safeStringify(123n);
+      expect(result).toBe("123n");
+    });
+
+    it("should return a string representation of a Set", () => {
+      const result = safeStringify(new Set([1, 2, 3]));
+      expect(result).toBe("Set (3) {1, 2, 3}");
+    });
+
+    it("should return a string representation of a Map", () => {
+      const result = safeStringify(
+        new Map([
+          ["key1", "value1"],
+          ["key2", "value2"],
+        ]),
+      );
+      expect(result).toBe("Map (2) {key1 => value1, key2 => value2}");
+    });
+
+    it("should return a string representation of a LinkedListNode", () => {
+      const node3 = new LinkedListNode(
+        3,
+        null,
+        { id: "node3", type: ArgumentType.LINKED_LIST },
+        "list3",
+        callstack,
+      );
+      const node2 = new LinkedListNode(
+        2,
+        node3,
+        { id: "node2", type: ArgumentType.LINKED_LIST },
+        "list2",
+        callstack,
+      );
+      const node1 = new LinkedListNode(
+        1,
+        node2,
+        { id: "node1", type: ArgumentType.LINKED_LIST },
+        "list1",
+        callstack,
+      );
+      const result = safeStringify(node1);
+      expect(result).toBe("[1 -> 2 -> 3]");
+    });
+
+    it("should return a string representation of an array", () => {
+      const result = safeStringify([1, "test", BigInt(3)]);
+      expect(result).toBe("[1, test, 3n]");
+    });
+
+    it("should return a string representation of an object with meta type BINARY_TREE", () => {
+      const binaryTreeNode = new BinaryTreeNode(
+        1,
+        null,
+        null,
+        {
+          id: "node1",
+          type: ArgumentType.BINARY_TREE,
+          depth: 0,
+        },
+        "tree1",
+        callstack,
+      );
+      const result = safeStringify(binaryTreeNode);
+      expect(result).toBe("Binary Tree [1]");
+    });
+
+    it("should return a string representation of an object with a non-finite number", () => {
+      const result = safeStringify({ value: Infinity });
+      expect(result).toBe('{\n  "value": "Inf"\n}');
+    });
+
+    it("should return a string representation of an object with a bigint", () => {
+      const result = safeStringify({ value: BigInt(123) });
+      expect(result).toBe('{\n  "value": "123n"\n}');
     });
   });
 });
