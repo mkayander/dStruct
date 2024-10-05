1dafaab95069e92d8e40573e223eef81b8ce475f
mkayander <maxim.kayander1@gmail.com>
2024-10-06 01:24:58 +0300

    feat: Added an app store selector

--- src/store/hooks/index.ts
+++ src/store/hooks/index.ts
@@ -2,9 +2,11 @@ import {
   type TypedUseSelectorHook,
   useDispatch,
   useSelector,
+  useStore,
 } from "react-redux";
 
 import type { AppDispatch, RootState } from "#/store/makeStore";
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppStore = () => useStore<RootState>();
